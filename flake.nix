93d3460a1687d73288708ffa47de55f8d5c3dc0d
Anders Lundstedt <git@anderslundstedt.se>
2025-01-21 21:52:03 +0100

    flake.nix

---
+++ flake.nix
@@ -0,0 +1,49 @@
+# flake based on
+# https://github.com/NixOS/templates/blob/ad0e221dda33c4b564fad976281130ce34a20cb9/bash-hello/flake.nix
+{
+  description = "CLI tool to check fonts' Unicode coverage";
+
+  inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+
+  outputs = inputs@{self, nixpkgs}: (
+    let
+      # to work with older version of flakes
+      lastModifiedDate =
+        self.lastModifiedDate or self.lastModified or "19700101";
+
+      # generate a user-friendly version number.
+      version = builtins.substring 0 8 lastModifiedDate;
+
+      # confirmed to work on the following systems
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      # helper function to generate an attrset
+      # '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      get-nixpkgs-for-system = system:
+        (import inputs.nixpkgs {inherit system;});
+
+      get-python-env-for-system = system: (
+        (get-nixpkgs-for-system system).python3.withPackages (
+          python-packages: [
+            python-packages.python-fontconfig
+          ]
+        )
+      );
+    in {
+      devShell = forAllSystems(system:
+        (get-nixpkgs-for-system system).mkShell {
+          buildInputs = [
+            (get-python-env-for-system system)
+          ];
+        }
+      );
+    }
+  );
+}
